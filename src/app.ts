b5bcdd95a621670b4c6081e29253966ac1ab6aa5
LG-Soria <lucas_s96@outlook.com>
2025-10-22 09:18:57 -0300

    api

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api", storeRoutes);
 app.use("/api/cajas", posRoutes);
 app.use("/api", ordersRoutes);
-app.use("/webhooks", webhooksRoutes);
+app.use("/api/webhooks", webhooksRoutes);
 app.get("/api/health", (_req, res) => res.status(200).send("ok"));
 
 export default app;
